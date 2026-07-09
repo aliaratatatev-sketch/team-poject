53ad6874251c13aa94053309da0ed66698ca9a54
Aliar <aliaratatatev@example.com>
2026-07-09 17:41:02 +0600

    Move hero section to home page

---
+++ team-project/src/assets/pages/home/home.jsx
@@ -0,0 +1,16 @@
+import './home.css'
+
+function Home() {
+  return (
+    <header className="hero">
+      <div className="hero__content">
+        <p className="hero__label">Автосалон</p>
+        <h1>Магазин по продаже машин</h1>
+        <p className="hero__slogan">Надёжные автомобили по выгодным ценам</p>
+        <button className="hero__button">Посмотреть каталог</button>
+      </div>
+    </header>
+  )
+}
+
+export default Home
